f22303e0a922250907ab06a8b1e5ddc62ee04d1b
santosh352y <santosh352y@gmail.com>
2023-10-25 22:34:51 +0530

    added sender confirmation

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         echo "Invalid email format. Please provide a valid email address.";
     } else {
-        // Send confirmation email
+        // Send confirmation email to you
         $to = "santosh352y@gmail.com"; // Updated with your actual email address
         $subject = "Contact Form Submission from Your Website";
         $message_body = "Name: $name\n"
@@ -18,7 +18,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $headers = "From: $email";
 
-        if (mail($to, $subject, $message_body, $headers)) {
+        // Send confirmation email to the sender
+        $sender_subject = "Confirmation of Your Form Submission";
+        $sender_message = "Dear $name,\n\nThank you for submitting the form. We have received your message and will get back to you soon.\n\nSincerely, YadavG.com";
+
+        // Send emails
+        if (mail($to, $subject, $message_body, $headers) && mail($email, $sender_subject, $sender_message)) {
             echo "Form submitted successfully. You will receive a confirmation email.";
             echo '<p>Redirecting to homepage in <span id="countdown">5</span> seconds.</p>';
             echo '<button id="redirectButton" onclick="window.location.href = \'index.html\';">Go to Homepage</button>';
